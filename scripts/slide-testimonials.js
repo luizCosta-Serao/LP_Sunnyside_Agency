0884771d3c771fe4ea85247415152232a574f51c
Luiz Serão <luiz.serao@hotmail.com>
2022-12-07 08:38:47 -0300

    atualização slide da section testimonials

--- scripts/slide-testimonials.js
+++ scripts/slide-testimonials.js
@@ -1,12 +1,11 @@
 export default function slideTestimonials() {
   const slideWrapper = document.getElementById("clients")
+  const slideItem = document.querySelectorAll(".clients li")
   const mediaMobile = window.matchMedia("(max-width:768px)")
   if(mediaMobile.matches === true) {
     const changeSlide = document.querySelectorAll(".change-slide span")
     changeSlide.forEach((slide, index) => {
 
-      const slideItem = document.querySelectorAll(".clients li")
-
       function changeSlide() {
         slideWrapper.style.transition = "transform .5s"
         if(slideItem[index] !== 0 ) {
@@ -16,43 +15,13 @@ export default function slideTestimonials() {
         }  
       }
       slide.addEventListener("click", changeSlide)
-
-
-      function reloadSlide() {
-        changeSlide()
-      }
-      window.addEventListener("resize", reloadSlide)
     })
   }
 
-
-  window.addEventListener("resize", () => {
-  if(mediaMobile.matches === true) {
-    const changeSlide = document.querySelectorAll(".change-slide span")
-    changeSlide.forEach((slide, index) => {
-
-      const slideItem = document.querySelectorAll(".clients li")
-
-      function changeSlide() {
-        slideWrapper.style.transition = "transform .5s"
-        if(slideItem[index] !== 0 ) {
-          slideWrapper.style.transform = `translateX(-${(slideItem[index].clientWidth + 30) * index}px)`
-        } else {
-          slideWrapper.style.transform = `translateX(-${slideItem[index].clientWidth * index}px)`
-        }  
-      }
-      slide.addEventListener("click", changeSlide)
-
-
-      function reloadSlide() {
-        changeSlide()
-      }
-      window.addEventListener("resize", reloadSlide)
-    })
-  } else {
-    window.addEventListener("resize", () => {
-        slideWrapper.style.transform = `translateX(0px)`
-    })
-  }
-})
+function reloadSlide() {
+  slideWrapper.style.transform = `translateX(-${slideItem[0].clientWidth * 0}px)`
+  console.log(slideItem[0])
+  console.log("OI")
+}
+window.addEventListener("resize", reloadSlide)
 }
