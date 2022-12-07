a3fdc806034bd1d66c33fd9738a7d7945e7f64fa
Luiz Serão <luiz.serao@hotmail.com>
2022-12-06 22:10:03 -0300

    Correção de bug no resize do slide na section testimonials

--- scripts/slide-testimonials.js
+++ scripts/slide-testimonials.js
@@ -1,24 +1,58 @@
 export default function slideTestimonials() {
-  const changeSlide = document.querySelectorAll(".change-slide span")
-  changeSlide.forEach((slide, index) => {
+  const slideWrapper = document.getElementById("clients")
+  const mediaMobile = window.matchMedia("(max-width:768px)")
+  if(mediaMobile.matches === true) {
+    const changeSlide = document.querySelectorAll(".change-slide span")
+    changeSlide.forEach((slide, index) => {
 
-    const slideWrapper = document.getElementById("clients")
-    const slideItem = document.querySelectorAll(".clients li")
+      const slideItem = document.querySelectorAll(".clients li")
 
-    function changeSlide() {
-      slideWrapper.style.transition = "transform .5s"
-      if(slideItem[index] !== 0 ) {
-        slideWrapper.style.transform = `translateX(-${(slideItem[index].clientWidth + 30) * index}px)`
-      } else {
-        slideWrapper.style.transform = `translateX(-${slideItem[index].clientWidth * index}px)`
-      }  
-    }
-    slide.addEventListener("click", changeSlide)
+      function changeSlide() {
+        slideWrapper.style.transition = "transform .5s"
+        if(slideItem[index] !== 0 ) {
+          slideWrapper.style.transform = `translateX(-${(slideItem[index].clientWidth + 30) * index}px)`
+        } else {
+          slideWrapper.style.transform = `translateX(-${slideItem[index].clientWidth * index}px)`
+        }  
+      }
+      slide.addEventListener("click", changeSlide)
 
 
-    function reloadSlide() {
-      changeSlide()
-    }
-    window.addEventListener("resize", reloadSlide)
-  })
+      function reloadSlide() {
+        changeSlide()
+      }
+      window.addEventListener("resize", reloadSlide)
+    })
+  }
+
+
+  window.addEventListener("resize", () => {
+  if(mediaMobile.matches === true) {
+    const changeSlide = document.querySelectorAll(".change-slide span")
+    changeSlide.forEach((slide, index) => {
+
+      const slideItem = document.querySelectorAll(".clients li")
+
+      function changeSlide() {
+        slideWrapper.style.transition = "transform .5s"
+        if(slideItem[index] !== 0 ) {
+          slideWrapper.style.transform = `translateX(-${(slideItem[index].clientWidth + 30) * index}px)`
+        } else {
+          slideWrapper.style.transform = `translateX(-${slideItem[index].clientWidth * index}px)`
+        }  
+      }
+      slide.addEventListener("click", changeSlide)
+
+
+      function reloadSlide() {
+        changeSlide()
+      }
+      window.addEventListener("resize", reloadSlide)
+    })
+  } else {
+    window.addEventListener("resize", () => {
+        slideWrapper.style.transform = `translateX(0px)`
+    })
+  }
+})
 }
